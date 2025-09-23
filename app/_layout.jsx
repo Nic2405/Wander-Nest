e52287c5399c2882f9bb93058a560c59118bae7e
Nic2405 <laarnienicoleprajes@gmail.com>
2025-09-23 12:59:43 +0800

    wandernest

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,44 +1,42 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
-import { useFonts} from 'expo-font'//add this import
-import { useEffect } from 'react'//add this import
-import { SplashScreen } from 'expo-router'//add this import
-SplashScreen.preventAutoHideAsync();// add this code after import
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
+import { SplashScreen } from 'expo-router'
+import '../firebaseConfig.js' // Initialize Firebase
+
+SplashScreen.preventAutoHideAsync();
 
 const MainLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
 
-  //add this code snippet right before “return”
-const [fontsLoaded, error] = useFonts({
-  "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-   "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
- 
-});
+  useEffect(() => {
+    if (error) throw error;
 
-useEffect(() => {
-  if (error) throw error;
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
 
-  if (fontsLoaded) {
-    SplashScreen.hideAsync();
+  if (!fontsLoaded && !error) {
+    return null;
   }
-}, [fontsLoaded, error]);
-
-if (!fontsLoaded && !error) {
-  return null;
-}
-
 
   return (
     <Stack>
-      <Stack.Screen 
-        name="index" 
-        options={{ 
+      <Stack.Screen
+        name="index"
+        options={{
           headerShown: false,
         }}
       />
-      <Stack.Screen 
-        name="profile" 
-        options={{ 
+      <Stack.Screen
+        name="profile"
+        options={{
           headerTitle: "Profile",
           headerShown: true,
         }}
